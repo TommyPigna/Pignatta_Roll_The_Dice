2efb07977220242cc3d127e288578f9850959582
TommyPigna <tommasopignatta@gmail.com>
2025-04-24 11:31:47 +0200

    Perfezionamento delle varie activity

--- app/src/main/java/com/example/pignatta_roll_the_dice/ThirdActivity.kt
+++ app/src/main/java/com/example/pignatta_roll_the_dice/ThirdActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pignatta_roll_the_dice
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,13 +15,29 @@ class ThirdActivity : AppCompatActivity() {
         setContentView(R.layout.activity_third)
         var numEstra = intent.getIntExtra("NUMESTRATTO", -1)
         var messWin = "";
+        var i=0;
         if (numEstra %2 == 0){
-            messWin = "IL NUMERO ESTRATTO E' $numEstra QUINDI HAI PERSO."
+            messWin = "IL NUMERO E' PARI QUINDI HAI PERSO."
         }
         else {
-            messWin = "IL NUMERO ESTRATTO E' $numEstra QUINDI HAI VINTO!"
+            messWin = "IL NUMERO E' DISPARI QUINDI HAI VINTO!!!"
         }
         val textView = findViewById<TextView>(R.id.textViewActivityThirdWin)
         textView.text = messWin;
+        var imageViewThird = findViewById<ImageView>(R.id.imageViewThirdActivity)
+
+
+        var imm = when(numEstra){
+            1 -> R.drawable.victory
+            2 -> R.drawable.lose
+            3 -> R.drawable.victory
+            4 -> R.drawable.lose
+            5 -> R.drawable.victory
+            6 -> R.drawable.lose
+            else -> {R.drawable.dices}
+        }
+        imageViewThird.setImageResource(imm)
     }
+
+
 }
